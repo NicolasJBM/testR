393e7aa0bcb9397fb361b9b8ecba4d658aaea54a
NicolasJBM <nicolas.jb.mangin@gmail.com>
2025-10-25 10:02:29 +0200

    FEAT: add a create problem function (modeled in create_essay)

---
+++ R/create_problem.R
@@ -0,0 +1,52 @@
+#' @name create_problem
+#' @title Create a problem
+#' @author Nicolas Mangin
+#' @description Function creating a problem question and retrieving correct answers from the propositions table.
+#' @param propositions Tibble. Table from which items (alternatives) and associated explanations are selected.
+#' @param codes Character string. Code of the question to which the propositions are linked.
+#' @param interrogation Character. Question asked to the student.
+#' @return Tibble. Table containing all the information about the propositions made to the student.
+#' @importFrom dplyr arrange
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom tibble tibble
+#' @export
+
+
+create_problem <- function(propositions, codes, interrogation){
+  
+  type <- NULL
+  language <- NULL
+  proposition <- NULL
+  value <- NULL
+  code <- NULL
+  retire <- NULL
+  
+  
+  exercise <- propositions |>
+    dplyr::filter(
+      type == "Essay",
+      code %in% codes,
+      retire == FALSE
+    )
+  
+  if (nrow(exercise) == 0){
+    exercise <- tibble::tibble(
+      item = "", code = "", type = "", document = "",
+      language = "", modifications = 0,
+      proposition = "", value = 1, scale = "",
+      keywords = ""
+    )
+  }
+  
+  exercise <- exercise |>
+    dplyr::arrange(proposition) |>
+    dplyr::mutate(
+      number = base::seq_len(base::nrow(exercise)),
+      letter = base::letters[base::seq_len(base::nrow(exercise))],
+      correct = value,
+      interrogation = interrogation
+    )
+  
+  return(exercise)
+}
